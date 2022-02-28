7c2a1b389c1e1a8921669aa213e0869bed582877
lmnghia911 <lmnghia911@gmail.com>
2022-02-28 11:55:41 +0700

    Add navbar

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Form\BookType;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,6 +27,7 @@ class BookController extends AbstractController
      */
     public function detail(Book $books): Response
     {
+        dump($books);
         return $this->render('book/detail.html.twig', [
             'book' => $books,
         ]);
@@ -38,6 +41,7 @@ class BookController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $bookRepo = $em->getRepository(Book::class);
         $book = $bookRepo->find($id);
+        dump($book);
         $em->remove($book);
         $em->flush();
 
@@ -49,4 +53,49 @@ class BookController extends AbstractController
         return $this->redirectToRoute('book_show');
 
     }
+
+    /**
+     * @Route("/book/edit/{id}", name="book_edit")
+     */
+    public function editBook($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $bookRepo = $em->getRepository(Book::class);
+        $book = $bookRepo->find($id);
+
+        $form = $this->createForm(BookType::class, $book);
+
+        if ($this->saveChanges($form, $request, $book)) {
+            $this->addFlash(
+                'notice',
+                'Book Edited'
+            );
+            return $this->redirectToRoute('book_show');
+        }
+
+        return $this->render('book/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    public function saveChanges($form, $request, $book)
+    {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book->setBookName($request->request->get('book')['book_name']);
+            $book->setDescription($request->request->get('book')['description']);
+            $book->setCategoryId($request->request->get('book')['category_id']);
+            $book->setAuthorId($request->request->get('book')['author_id']);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            return true;
+        }
+        return false;
+    }
+
+
 }
