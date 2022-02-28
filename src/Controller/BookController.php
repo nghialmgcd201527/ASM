26252292a4285860d369de69d040e5162a8481fa
lmnghia911 <lmnghia911@gmail.com>
2022-02-28 10:53:50 +0700

    Detail of book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +17,16 @@ class BookController extends AbstractController
     {
         return $this->render('book/index.html.twig', [
             'books' => $bookRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/book/{id}", name="book_detail")
+     */
+    public function detail(Book $books): Response
+    {
+        return $this->render('book/detail.html.twig', [
+            'book' => $books,
         ]);
     }
 }
