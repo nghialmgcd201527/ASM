6b1a1bb7dfa4ecff2866bb251a9a7c25de03c7e0
lmnghia911 <lmnghia911@gmail.com>
2022-03-06 11:53:59 +0700

    edit view

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,4 +22,5 @@ class UserController extends AbstractController
             'userInfor' => $user,
         ]);
     }
+
 }
